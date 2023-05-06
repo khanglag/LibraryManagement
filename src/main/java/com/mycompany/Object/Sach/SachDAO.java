978b7b6e2e2dfebdcd84727cd7973b6406305526
minnfeed <93919572+minnfeed@users.noreply.github.com>
2023-05-06 23:34:15 +0700

    Béo

--- src/main/java/com/mycompany/Object/Sach/SachDAO.java
+++ src/main/java/com/mycompany/Object/Sach/SachDAO.java
@@ -7,6 +7,7 @@ import ConnectDB.ConnectDB;
 import java.util.ArrayList;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import javax.swing.JOptionPane;
 /**
  *
  * @author pc
@@ -15,128 +16,98 @@ public class SachDAO {
     
     public SachDAO(){
     }
-    
     ConnectDB connectDB;
-    ArrayList<Sach> dsSach= new ArrayList<Sach>();
+    ArrayList<Sach> dss= new ArrayList<>();
     public ArrayList<Sach> readDB(){
         connectDB= new ConnectDB();
         try {
-            String qry="SELECT * FROM SACH WHERE TONTAI = 1";
+            String qry="SELECT *FROM SACH";
             ResultSet rset= connectDB.sqlQuery(qry);
            if(rset!=null){
                while(rset.next()){
-                   Sach sach= new Sach(rset.getNString("MASA"),
-                           rset.getNString("TENSA"), rset.getNString("TT"),rset.getNString("SA_MALOAI"),
-                           rset.getNString("MATG"),rset.getNString("MANXB"),rset.getInt("SOTRANG"),
-                           rset.getInt("LXB"), rset.getInt("SOLUONG"), rset.getFloat("gia"), 
-                           rset.getBoolean("TONTAI"), rset.getNString("MA_ANH"));
-                           
-                   dsSach.add(sach);
+                   Sach sa= new Sach(rset.getNString("MASA"),rset.getNString("TENSA"),rset.getNString("TT"),rset.getNString("SA_MALOAI"), rset.getNString("MATG"), rset.getNString("MANXB"),
+            rset.getInt("SOTRANG") , 0, rset.getInt("SOTRANG"), rset.getFloat("gia"), rset.getInt("TONTAI"),rset.getNString("MA_ANH"));
+                   dss.add(sa);
                }
            }
         } catch (SQLException e) {
-            System.out.println(e);
+            System.out.println("Loi");
         }
         connectDB.closeConnect();
-        return dsSach;
+        return dss;
     }
-    public Boolean add(Sach sach){
+    public Boolean add(Sach sa){
         connectDB= new ConnectDB();
-        boolean success= connectDB.sqlUpdate("insert into SACH(MASA,TENSA,TT,MATG,SA_MALOAI,MANXB,MA_ANH,LXB,SOTRANG,SOLUONG,gia,TONTAI) values('"
-                +sach.getMaSach()+"','"
-                +sach.getTenSach()+"','"
-                +sach.getTinhTrang()+"','"
-                +sach.getMaTacGia()+"','"
-                +sach.getMaTheLoai()+"','"
-                +sach.getMaNXB()+"','"
-                +sach.getAnh()+"','"
-                +sach.getLanXuatBan()+"','"
-                +sach.getSoTrang()+"','"
-                +sach.getSoLuong()+"','"
-                +sach.getGia()+"','"
-                +",'"+1+"')");
+        boolean success= connectDB.sqlUpdate("insert into SACH(MASA,TENSA,TT,SOTRANG,SOLUONG,gia,SA_MALOAI,MATG,MANXB,MA_ANH,TONTAI) values('"
+                +sa.getMaSach()+"','"
+                +sa.getTenSach()+"','"
+                +sa.getTinhTrang()+"','"
+                +sa.getSoTrang()+"','"
+                +sa.getSoLuong()+"','"
+                +sa.getGia()+"','"
+                +sa.getMaTheLoai()+"','" 
+                +sa.getMaTacGia()+"','"
+                +sa.getMaNXB()+"','"
+                +sa.getAnh()+"','"
+                +sa.isTonTai()+"')");
         connectDB.closeConnect();
         return success;
     }
-    public boolean delete(Sach sach) {
+    public boolean delete(Sach sa){
         connectDB = new ConnectDB();
-        boolean success = connectDB
-                .sqlUpdate("UPDATE SACH SET TONTAI = 0 WHERE MASA ='" + sach.getMaSach() + "'");
-        
+        boolean success= connectDB.sqlUpdate("UPDATE SACH SET TONTAI = '1' WHERE MASA ='"+sa.getMaSach()+"'");
         connectDB.closeConnect();
         return success;
     }
-    public boolean update(Sach sach) {
+    public boolean update(Sach sa){
         connectDB = new ConnectDB();
-        boolean success = connectDB.sqlUpdate("UPDATE SACH SET TENSA='" + sach.getTenSach()
-                + "', TT='" + sach.getTinhTrang()
-                + "', SOLUONG='" + sach.getSoLuong()
-                + "', gia='" + sach.getGia()
-                + "', MA_ANH='" + sach.getAnh()
-                + "' WHERE MASA ='" + sach.getMaSach() + "'");
+        boolean success= true;
+        connectDB.sqlUpdate("UPDATE DOCGIA SET TENSA+'"+sa.getTenSach()
+                +"', TT='"+sa.getTinhTrang()
+                +"', SOTRANG='"+sa.getSoTrang()
+                +"', SOLUONG='"+sa.getSoLuong()
+                +"', gia='"+sa.getGia()
+                +"', SA_MALOAI='"+sa.getMaTheLoai()
+                 +"', MATG='"+sa.getMaTacGia()
+                 +"', MANXB='"+sa.getMaTheLoai()
+                 +"', MA_ANH='"+sa.getAnh()
+                +"', TONTAI='"+sa.isTonTai()
+                +"' WHERE MADG ='"+sa.getMaSach()+"'");
         connectDB.closeConnect();
         return success;
     }
-    public boolean hasID(String id) {
-        boolean result = false;
-        connectDB = new ConnectDB();
-
-        try {
-            String qry = "Select * from sach where masa='" + id + "' and tontai = 1";
-            ResultSet rset = connectDB.sqlQuery(qry);
-            result = rset.next();
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-
-        return result;
-
-    }
-    public ArrayList<Sach> search(String maSach, String tenSach, String tinhTrang, String theLoai, String maTacGia, String maNXB, String anh, String soTrang, String lanXuatBan, String soLuong, String gia){
+    public ArrayList<Sach> timSachs(String maSach, String tenSach, String tinhTrang, String maTheLoai, String maTacGia, String maNXB
+          ){
         connectDB = new ConnectDB();
         ArrayList<Sach> ketqua =new ArrayList<Sach>();
-        String qry="SELECT * FROM SACH WHERE TONTAI = 1";
-        if(maSach.equals("") == false) 
+        String qry="SELECT *FROM SACH WHERE TONTAI=1 ";
+        if(maSach!=null) 
             qry+=(" AND MASA= '"+maSach+ "'");
-        if(tenSach.equals("") == false) 
+        if(tenSach!=null) 
             qry+=(" AND TENSA= '"+tenSach+ "'");
-        if(tinhTrang.equals("") == false) 
+        if(tinhTrang!=null) 
             qry+=(" AND TT= '"+tinhTrang+ "'");
-        if(theLoai.equals("") == false) 
-            qry+=(" AND SA_MALOAI= '"+theLoai+ "'");
-        if(maTacGia.equals("") == false) 
-            qry+=(" AND MATG= '"+maTacGia+ "'");
-        if(maNXB.equals("") == false) 
+        if(maTheLoai!=null) 
+            qry+=(" AND SA_MALOAI= '"+maTheLoai+ "'");
+        if(maNXB!=null) 
             qry+=(" AND MANXB= '"+maNXB+ "'");
-        if(anh.equals("") == false) 
-            qry+=(" AND MA_ANH= '"+anh+ "'");
-        if(soTrang.equals("") == false) 
-            qry+=(" AND SOTRANG= "+Integer.parseInt(soTrang));
-        if(lanXuatBan.equals("") == false) 
-            qry+=(" AND LXB= "+Integer.parseInt(lanXuatBan));
-        if(soLuong.equals("") == false) 
-            qry+=(" AND SOLUONG= "+Integer.parseInt(soLuong));
-        if(gia.equals("") == false) 
-            qry+=(" AND gia= "+Float.parseFloat(gia));
-       
+        if(maTacGia!=null) 
+            qry+=(" AND MATG= '"+maTacGia+ "'");
         System.out.println(qry);
         ResultSet rset= connectDB.sqlQuery(qry);
         try {
-            if (rset!=null) {
-                  while(rset.next()){
-                   Sach sach= new Sach(rset.getNString("MASA"),
-                           rset.getNString("TENSA"),rset.getNString("TT"),rset.getNString("SA_MALOAI"),
-                           rset.getNString("MATG"),rset.getNString("MANXB"),rset.getInt("SOTRANG"),
-                           rset.getInt("LXB"),rset.getInt("SOLUONG"),rset.getFloat("gia"),rset.getBoolean("TONTAI"),
-                           rset.getNString("MA_ANH"));
-                   ketqua.add(sach);
+            if(rset!=null){
+               while(rset.next()){
+                   Sach sa= new Sach(rset.getNString("MASA"),rset.getNString("TENSA"),rset.getNString("TT"),rset.getNString("SA_MALOAI"), rset.getNString("MATG"), rset.getNString("MANXB"),
+            rset.getInt("SOTRANG") , 0, rset.getInt("SOTRANG"), rset.getFloat("gia"), rset.getInt("TONTAI"),rset.getNString("MA_ANH"));
+                   ketqua.add(sa);
                }
-            }
+           }
         } catch (SQLException e) {
-            System.out.println(e);
+             JOptionPane.showMessageDialog(null,"Không có đối tượng cần tìm");
         }
         connectDB.closeConnect();
         return ketqua;
 }
-     
+    
 }
