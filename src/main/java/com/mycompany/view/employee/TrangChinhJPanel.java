9636bac98d9d50382719327850bb09da98198e06
Minh Khang <khangminh.do2003@gmail.com>
2023-05-15 15:45:17 +0700

    fn

--- src/main/java/com/mycompany/view/employee/TrangChinhJPanel.java
+++ src/main/java/com/mycompany/view/employee/TrangChinhJPanel.java
@@ -47,12 +47,27 @@ public class TrangChinhJPanel extends javax.swing.JPanel {
         jLabelTen = new javax.swing.JLabel();
 
         setBackground(new java.awt.Color(51, 51, 51));
-        setLayout(new java.awt.GridLayout());
 
         jLabelTen.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabelTen.setForeground(new java.awt.Color(255, 255, 255));
         jLabelTen.setText("jLabel1");
-        add(jLabelTen);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabelTen, javax.swing.GroupLayout.PREFERRED_SIZE, 449, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(31, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(126, 126, 126)
+                .addComponent(jLabelTen, javax.swing.GroupLayout.PREFERRED_SIZE, 42, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(144, Short.MAX_VALUE))
+        );
     }// </editor-fold>//GEN-END:initComponents
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
